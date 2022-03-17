17ea66bc7e653b9eae779dd74aea8111868dd35b
Juan Gabriel Gonzalez <juan.g.gonzalez13400@gmail.com>
2022-03-17 11:37:52 -0300

    second commit

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usRouter = require('./routes/us');
 var newsRouter = require('./routes/news');
 var contactRouter = require('./routes/contact');
-
+var loginRouter = require('./routes/pseudo_login')
 var app = express();
 
 // view engine setup
@@ -22,11 +23,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret:'12345678912345678912',
+  resave:false,
+  saveUninitialized:true
+}))
+
+app.get('/pseudo_login', function (req,res){
+  var conocido = Boolean(req.session.nombre)
+
+  res.render('pseudo_login',{
+    title:'sessiones en express',
+    conocido: conocido,
+    nombre: req.session.nombre
+  })
+})
+
+app.post('/pseudo_login',function(req,res){
+  if(req.body.nombre){
+    req.session.nombre = req.body.nombre
+  }
+  res.redirect('/pseudo_login')
+})
+
+app.get('/salir',function(req,res){
+  req.session.destroy();
+  res.redirect('/pseudo_login')
+})
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/us',usRouter);
 app.use('/news',newsRouter);
 app.use('/contact',contactRouter);
+app.use('/pseudo_login',loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
